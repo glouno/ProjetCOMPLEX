c8e44542cf5b48b9d90afb7aefb8b1fdcf6503f6
Tudor Enache <tudor.enache7@gmail.com>
2024-11-27 17:40:11 +0100

    Rewrote Paul's code; defined Tree structure and some basic methods for it.

---
+++ src/cartesian_tree.c
@@ -0,0 +1,97 @@
+// Code for creating and working with cartesian trees
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// node structure
+typedef struct Node {
+    char key;
+    double priority;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+// tree structure
+typedef struct Tree {
+    Node* root;
+} Tree;
+
+// safe to free NULL pointers, so we only consider creation functions
+// functions for managing the creation of nodes
+Node* create_node(char key, int priority) {
+    Node* new_node = (Node*)malloc(sizeof(Node));
+
+    if (new_node == NULL) {
+        printf("Error in allocating memory for a new node.");
+        return NULL;
+    }
+
+    new_node->key = key;
+    new_node->priority = priority;
+    new_node->left = NULL;
+    new_node->right = NULL;
+
+    return new_node;
+}
+
+// functions for managing the creation and deletion of trees
+Tree* create_empty_tree() {
+    Tree* tree = (Tree*)malloc(sizeof(Tree));
+
+    if (tree == NULL) {
+        printf("Error in allocating memory for a new tree.");
+        return NULL;
+    }
+
+    return tree;
+}
+
+void free_tree(Node* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    free_tree(root->left);
+    free_tree(root->right);
+    free_tree(root);
+}
+
+void delete_tree(Tree* tree) {
+    if (tree != NULL) {
+        free_tree(tree->root);
+        free(tree);
+    }
+}
+
+// utility functions
+int is_empty(Tree* tree) {
+    return (tree->root == NULL);
+}
+
+Node* get_left_child(Node* node) {
+    if (node != NULL) {
+        return node->left;
+    }
+    return NULL;
+}
+
+Node* get_right_child(Node* node) {
+    if (node != NULL) {
+        return node->right;
+    }
+    return NULL;
+}
+
+void add_left_child(Node* parent, Node* child) {
+    if (parent != NULL) {
+        parent->left = child;
+    }
+    return;
+}
+
+void add_right_child(Node* parent, Node* child) {
+    if (parent != NULL) {
+        parent->right = child;
+    }
+    return;
+}
